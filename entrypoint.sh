f93521756dfb219061d541a78c8bc5e7fec405b8
Florian Dorau <fdorau@it-economics.de>
2019-03-20 10:41:39 +0100

    Use args instead of env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
-#!/bin/sh -l
+#!/bin/sh
 
-file=$JQ_FILE 
-options=$JQ_OPTIONS
-
-if [ -z "$JQ_FILTERS" ];
-then filters=".";
-else filters="$JQ_FILTERS";
-fi 
-
-echo "Running jq with options '$options' and filters '$filters' for file '$file'."
-result=$(jq $options $filters $file)
+result=$(sh -c "jq $*")
 
 export JQ_OUTPUT=$result;
 
